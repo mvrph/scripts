25045ad944244b49ba782f3a4a9bfe344819c691
also joe <joe@deepvoid.ai>
2025-02-24 23:40:35 -0800

    Create create_aliases.sh

---
+++ create_aliases.sh
@@ -0,0 +1,5 @@
+echo "alias sleep='/Users/joe/dev/scripts/turn_osx_monitors_off.sh'
+# alias add='dummy_command'
+# alias auto_start_vlc_fullscreen='dummy_command'
+# alias turn_windows_montiors_off='dummy_command'
+# alias windows_ssh_enable='dummy_command'" >> ~/.zshrc && source ~/.zshrc
